3b88d8a840523bb3768845da8e200f3236a11c28
Harvey Ezihe <don4dolex@gmail.com>
2023-09-26 12:39:43 +0100

    Implement login logic

--- crm_website/views.py
+++ crm_website/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-
+# Mixture of login/home page. Template in home.html
 def home(request):
-    return render(request, 'home.html', {})
+    # Check to see if user is logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenticate User
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login Successful")
+            return redirect('home')
+        else:
+            messages.success(
+                request, "There was an error, please try again...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+def login_user(request):
+    pass
+
+
+def logout_user(request):
+    pass
