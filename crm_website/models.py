7ba33795767d007acc5284b36e863b4749a54e71
Harvey Ezihe <don4dolex@gmail.com>
2023-09-27 18:03:01 +0100

    Create database schema for customers, add dummy records

--- crm_website/models.py
+++ crm_website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Any type of database you use will work here with this same code
+
+# Django pluralizes the db name. Leave it as singular
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    phone_number = models.CharField(max_length=25)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    postcode = models.CharField(max_length=50)
+
+    # Default values to return when you query a user
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name} {self.email}")
