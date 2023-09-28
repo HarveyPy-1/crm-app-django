78741d1c4afbe5f78e4b6816ebcd602d1e46b09b
Harvey Ezihe <don4dolex@gmail.com>
2023-09-28 10:24:31 +0100

    Add add record functionality

--- crm_website/views.py
+++ crm_website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .form import SignUpForm
+from .form import SignUpForm, AddRecordForm
 from .models import Record
 
 # Mixture of login/home page, called conditionally
@@ -64,3 +64,23 @@ def customer_record(request, pk):
     else:
         messages.success(request, "You must be logged in...")
         return redirect ('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_item = Record.objects.get(id=pk)
+        delete_item.delete()
+        messages.success(request, "Successfully deleted")
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, "You must be logged in")
+        return redirect('home')
